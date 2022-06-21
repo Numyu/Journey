ba45e95432881e6806f792b1a3abdc8d28faf9a2
Léa M <mhoumadi.lea@gmail.com>
2022-06-21 14:44:45 +0200

    artisan card structure

--- src/Components/ArtisanCard/ArtisanCard.js
+++ src/Components/ArtisanCard/ArtisanCard.js
@@ -1,18 +1,35 @@
-import React, {useState } from 'react'
+import React, { useState } from "react";
 
-export default function ArtisanCard({shopName, shopDetail, shopDescription, openingHours}) {
+export default function ArtisanCard({
+  shopName,
+  shopDetail,
+  shopDescription,
+  openingHours,
+}) {
+  const [cardToggled, setCardToggled] = useState(false);
 
-     const [cardToggled, setCardToggled ] = useState(false)
+  const toggledCardDescription = () => {
+    setCardToggled(!cardToggled);
+  };
 
-     const toggledCardDescription = () => {
-        setCardToggled(!cardToggled)
-     }
-     
   return (
     <div
-    className={cardToggled ? "artisan-card toggled" : "artisan-card"}
-    onClick={toggledCardDescription}
+      className={cardToggled ? "artisan-card toggled" : "artisan-card"}
+      onClick={toggledCardDescription}
     >
+      <div className="artisan-card-header">
+        <p className="artisan-openingHours">{openingHours}</p>
+        <h3 className="artisan-card-title">{shopName}</h3>
+        <p className="artisan-card-detail">{shopDetail}</p>
+      </div>
+
+        {cardToggled && (
+            <div className="artisan-card-body">
+                <p className="artisan-openingHours">{openingHours}</p>
+                <p className="artisan-shop-description">{shopDescription}</p>
+            </div>
+        )}
+
     </div>
-  )
+  );
 }
