6b0dec640bc56e85704c1dda33224ca44fb7cb09
Louis R <95973810+LouisSleep@users.noreply.github.com>
2022-06-21 15:28:45 +0200

    feat MostViewedCard

--- src/Components/MostViewed/ItemCard.js
+++ src/Components/MostViewed/ItemCard.js
@@ -3,12 +3,14 @@ import React from 'react'
 export default function ItemCard(props) {
     return (
 
-        <>
-            <button className={props.classCard} onClick={() => { props.Navigate(props.id) }}>
+        <><div className={props.classCard}>
+            <button onClick={() => { props.Navigate(props.id) }}>
             </button>
             <h3>
                 {props.title}
             </h3>
+        </div>
+
         </>
 
     )
--- src/Components/MostViewed/MostViewed.js
+++ src/Components/MostViewed/MostViewed.js
@@ -8,31 +8,36 @@ export default function MostViewed() {
 
 
     const [cardItem, setCard] = useState([
-        { id: 1, title: 'Eiffel Tower', classCard: 'EiffelTowerCard', img: '../../img/MostViewedImg/eiffel.png' },
-        { id: 2, title: 'Louvre Museum', classCard: 'LouvreMuseumCard', img: '../../img/MostViewedImg/eiffel.png' },
-        { id: 3, title: 'Triumph Arch', classCard: 'TriumphArchCard', img: '../../img/MostViewedImg/eiffel.png' },
-        { id: 4, title: 'Sacré-Coeur’s Basilica', classCard: 'SacreCoeursBasilica', img: '../../img/MostViewedImg/eiffel.png' }
+        { id: 1, title: 'Eiffel Tower', classCard: 'eiffel-tower-card', img: '../../img/MostViewedImg/eiffel.png' },
+        { id: 2, title: 'Louvre Museum', classCard: 'louvre-museum-card', img: '../../img/MostViewedImg/eiffel.png' },
+        { id: 3, title: 'Triumph Arch', classCard: 'triumph-arch-card', img: '../../img/MostViewedImg/eiffel.png' },
+        { id: 4, title: 'Sacré-Coeur’s Basilica', classCard: 'sacre-coeurs-basilica', img: '../../img/MostViewedImg/eiffel.png' }
     ])
 
 
     return (
         <>
             <h2>MostViewed</h2>
-            <ul>
-                {
-                    cardItem.map((item, index) => {
-                        return (
-                            <ItemCard
-                                Key={index}
-                                classCard={item.classCard}
-                                img={item.img}
-                                title={item.title}
-                            />
-                        )
+            <div className='most-viewed-content'>
+                <ul>
 
-                    })
-                }
-            </ul>
+
+                    {
+                        cardItem.map((item, index) => {
+                            return (
+                                <ItemCard
+                                    Key={index}
+                                    classCard={item.classCard}
+                                    img={item.img}
+                                    title={item.title}
+                                />
+                            )
+
+                        })
+                    }
+                </ul>
+            </div>
+
         </>
 
     )
