6e51585f2c1cae19e98fc4a320a755ba67300a83
Louis R <95973810+LouisSleep@users.noreply.github.com>
2022-06-21 14:39:13 +0200

    feat MostViewed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import MostViewed from './Components/MostViewed/MostViewed';
 
 function App() {
   return (
     <div className="App">
+      <MostViewed />
     </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import MostViewed from './Components/MostViewed/MostViewed';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
-        <MostViewed />
+        <App />
     </Router>
 );
 
