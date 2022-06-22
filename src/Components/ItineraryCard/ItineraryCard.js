94a43a07638f14a3dad2e9002099ca27715577eb
Billy IEA <iea.billy.kd@gmail.com>
2022-06-23 00:11:13 +0200

    feat: generate itinerary card (category, name, description)

--- src/Components/ItineraryCard/ItineraryCard.js
+++ src/Components/ItineraryCard/ItineraryCard.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import "./ItineraryCard.css"
 
-export default function ItineraryCard() {
+export default function ItineraryCard(props) {
   return (
-    <div>
-        <h1>ItineraryCard</h1>
+    <div className={`itinerary-card ${ props.id }`}>
+      <div className='itinerary-content'>
+        <p className='itinerary-category'>{props.category.toUpperCase()}</p>
+
+        <h3 className='itinerary-name'>{props.itineraryName}</h3>
+        <p className='itinerary-description'>{props.description}</p>
+      </div>
+        
     </div>
   )
 }
--- src/Pages/ItineraryPage/ItineraryPage.js
+++ src/Pages/ItineraryPage/ItineraryPage.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import ItineraryCard from '../../Components/ItineraryCard/ItineraryCard'
+import { useState } from 'react'
 
 export default function ItineraryPage() {
+  const [itineraries, setItineraries] = useState([
+    {category: "global", itineraryName: "Discover", description: "Enjoy the best local craftmanship", id: "discover"},
+    {category: "global", itineraryName: "Paris Lover", description: "Live the 'art de vivre à la Française'", id: "paris-lover"},
+    {category: "global", itineraryName: "Grand Tour", description: "Experience French Luxury", id: "grand-tour"},
+  ])
   return (
-    <>
-    <ItineraryCard />
-    </>
+    <ul>
+      {itineraries.map((item, index) => {
+        return (
+          <ItineraryCard 
+            key={index}
+            category={item.category}
+            itineraryName={item.itineraryName}
+            description={item.description}
+            id={item.id}
+          />
+        )
+      })}
+    </ul>
+    
+    
+    
   )
 }
