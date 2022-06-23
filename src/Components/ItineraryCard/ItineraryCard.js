5d51d2a5b33a6fce9b12c8b1f2f5d90decb02e6d
Billy IEA <iea.billy.kd@gmail.com>
2022-06-23 16:58:51 +0200

    feat: content + itinerary image + responsive

--- src/Components/ItineraryCard/ItineraryCard.js
+++ src/Components/ItineraryCard/ItineraryCard.js
@@ -2,15 +2,17 @@ import React from 'react'
 import "./ItineraryCard.css"
 
 export default function ItineraryCard(props) {
+  const itineraryImg = `linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${ props.id }.png)`
   return (
-    <div className={`itinerary-card ${ props.id }`}>
+    <div className={`itinerary-card ${ props.id }`} style={{backgroundImage: `${ itineraryImg }`}}>
+      <div className='it-filter'></div>
       <div className='itinerary-content'>
         <p className='itinerary-category'>{props.category.toUpperCase()}</p>
 
         <h3 className='itinerary-name'>{props.itineraryName}</h3>
         <p className='itinerary-description'>{props.description}</p>
       </div>
-        
+          
     </div>
   )
 }
--- src/Pages/ItineraryPage/ItineraryPage.js
+++ src/Pages/ItineraryPage/ItineraryPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ItineraryCard from '../../Components/ItineraryCard/ItineraryCard'
 import { useState } from 'react'
+import "./ItineraryPage.css"
 
 export default function ItineraryPage() {
   const [itineraries, setItineraries] = useState([
@@ -8,22 +9,34 @@ export default function ItineraryPage() {
     {category: "global", itineraryName: "Paris Lover", description: "Live the 'art de vivre à la Française'", id: "paris-lover"},
     {category: "global", itineraryName: "Grand Tour", description: "Experience French Luxury", id: "grand-tour"},
   ])
+
+  const ctaBack = <svg xmlns="http://www.w3.org/2000/svg" width="28" height="18" fill="none" viewBox="0 0 28 18">
+    <path stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m26.006 8.513-24.944.112m6.502-7.517L1.153 8.64l6.659 7.413"/>
+    </svg>
+
   return (
-    <ul>
-      {itineraries.map((item, index) => {
-        return (
-          <ItineraryCard 
-            key={index}
-            category={item.category}
-            itineraryName={item.itineraryName}
-            description={item.description}
-            id={item.id}
-          />
-        )
-      })}
-    </ul>
-    
-    
-    
+    <div className='itinerary-page-container'>
+      <button className='cta-back'>{ctaBack}</button>
+
+      <h2 className='itinerary-page-title'>Sacré-Coeur</h2>
+      <p className='itinerary-page-text'>Pick an itinary that suits you</p>
+
+      <ul className="itinerary-container">
+        
+        {itineraries.map((item, index) => {
+          return (
+            <ItineraryCard 
+              key={index}
+              category={item.category}
+              itineraryName={item.itineraryName}
+              description={item.description}
+              id={item.id}
+            />
+          )
+        })}
+      </ul>
+
+      <h2 className='itinerary-page-title filter-title'>Or sort by filter</h2>
+    </div>
   )
 }
