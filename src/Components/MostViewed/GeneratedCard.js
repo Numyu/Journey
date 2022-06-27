333f5d34df383379b136f6ab2f581a55e72c3a52
Léa M <mhoumadi.lea@gmail.com>
2022-06-27 14:49:58 +0200

    fix landing page pour ajouter au sw ensuite

--- src/Components/MostViewed/GeneratedCard.js
+++ src/Components/MostViewed/GeneratedCard.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-export default function GeneratedCard(props) {
-  const backGround = `no-repeat center url(${props.img}) `;
-  console.log(props.img);
+
+export default function GeneratedCard({classCard, img, title}) {
+  const backGround = {
+    background:`no-repeat center url(${img})`,
+    backgroundSize: "cover"
+  }
   return (
-    <div className={props.classCard} id={props.id}>
+    <div className={classCard}>
       <div className="card-mosaic">
-        <button
-          style={{ background: backGround, backgroundSize: "cover" }}
-          onClick={() => {
-            props.Navigate(props.id);
-          }}
+        <button className="mostViewedBtn"
+          style={backGround}
         ></button>
-        <h3>{props.title}</h3>
+        <h3>{title}</h3>
       </div>
     </div>
   );
--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import MostViewed from '../../Components/MostViewed/MostViewed'
 import SearchBar from '../../Components/SearchBar/SearchBar'
-import CtaMaps from '../../Components/CtaMaps/CtaMaps'
 
 export default function LandingPage() {
     return (
         <>
             <SearchBar />
             <MostViewed />
-            <CtaMaps />
         </>
     )
 }
