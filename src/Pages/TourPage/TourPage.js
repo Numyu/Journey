975c32007215e1fa7a2ebc2200b2887933af2f01
Anthony-Rgs <95974061+Anthony-Rgs@users.noreply.github.com>
2022-06-28 21:51:45 +0200

    feat: api data

--- src/Pages/TourPage/TourPage.js
+++ src/Pages/TourPage/TourPage.js
@@ -1,97 +1,69 @@
-import { useState, useEffect } from "react";
-import ArtisanCard from "../../Components/ArtisanCard/ArtisanCard";
-import api from "../../api/api.json";
-import CtaBack from "../../Components/CtaBack/CtaBack";
-import { useParams } from "react-router-dom";
-import "./TourPage.css"
-import CtaMaps from "../../Components/CtaMaps/CtaMaps";
+import { useState, useEffect } from 'react'
+import ArtisanCard from '../../Components/ArtisanCard/ArtisanCard'
+import api from '../../api/api.json'
+import CtaBack from '../../Components/CtaBack/CtaBack'
+import { useParams } from 'react-router-dom'
+import './TourPage.css'
+import CtaMaps from '../../Components/CtaMaps/CtaMaps'
 
 export default function TourPage() {
-  const [stores, setStores] = useState([]);
-
+  const [stores, setStores] = useState([])
 
   useEffect(() => {
-    setStores(api);
-  }, []);
-
-
+    setStores(api)
+  }, [])
 
   const params = useParams()
 
-
-
   const deleteArtisanCard = (uuid) => {
     const newStores = stores.filter((store) => {
-      return store.uuid != uuid;
-    });
-    setStores(newStores);
-  };
-
-
-
-
+      return store.uuid != uuid
+    })
+    setStores(newStores)
+  }
 
   const filteredStores = stores.filter((store) => {
-
-      return store.path == params.tour
+    return store.path == params.tour
   })
 
-
-
-
-
-  
   return (
     <div>
-
-      <div className='tour-page-header'>
-        <CtaBack
-    
-        valueLink={`/itineraries/${params.monument}`}
-          
-        />  
+      <div className="tour-page-header">
+        <CtaBack valueLink={`/itineraries/${params.monument}`} />
         {/* <button className='cta-back' onClick={() => navigate("/")}>{ctaBack} </button> */}
-        <h2 className='tour-page-title'>{params.tour}</h2>
-        <p className='tour-page-text'>Personalize your itinerary</p>
+        <h2 className="tour-page-title">{params.tour}</h2>
+        <p className="tour-page-text">Personalize your itinerary</p>
       </div>
-      
 
-
-      {filteredStores
-        .map(
-          ({
-            uuid,
-            name,
-            smallDescription,
-            opening,
-            description,
-            type,
-            image
-            
-          }) => (
-            <ArtisanCard
-              key={uuid}
-              uuid={uuid}
-              shopName={name}
-              shopDetail={smallDescription}
-              openingHours={opening}
-              shopDescription={
-                description ? description : "Il n'y a pas de description"
-              }
-              type_de_commerce={type}
-
-              deleteArtisanCard={deleteArtisanCard}
-              picture={
-                process.env.PUBLIC_URL + "/images/ArtisanCardImg/" + image
-              }
-            />
-          )
-        )}
-        {/* <CtaMaps 
+      {filteredStores.map(
+        ({
+          uuid,
+          name,
+          smallDescription,
+          opening,
+          description,
+          type,
+          image,
+        }) => (
+          <ArtisanCard
+            key={uuid}
+            uuid={uuid}
+            shopName={name}
+            shopDetail={smallDescription}
+            openingHours={opening}
+            shopDescription={
+              description ? description : "Il n'y a pas de description"
+            }
+            type_de_commerce={type}
+            deleteArtisanCard={deleteArtisanCard}
+            picture={process.env.PUBLIC_URL + '/images/ArtisanCardImg/' + image}
+          />
+        ),
+      )}
+      {/* <CtaMaps 
         
         />
          */}
-        
     </div>
-  );
+  )
 }
